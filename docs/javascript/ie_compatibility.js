976f20d2ead66c81c734d57c56ba054415f81258
Mike Kim <nser789@gmail.com>
2021-10-07 20:44:30 +0900

    우리집 인터넷 익스플로러에서는 링크가 다 평문처럼 보인다. #60

---
+++ docs/javascript/ie_compatibility.js
@@ -0,0 +1,1 @@
+var agent = navigator.userAgent.toLowerCase(); if ( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) ) {document.write("<link rel='stylesheet' href='stylesheets/ie_compatibility.css'>");}
